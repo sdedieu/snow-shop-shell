f7e409367a113bced55594c82e21e700b5b9e9a5
glsm4tt <appstrophe@gmail.com>
2021-08-22 09:06:49 +0200

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundSize: (theme) => ({
+        100: "100%",
+      }),
+      backgroundImage: (theme) => ({
+        "sales-image": "url('/assets/img/pexels-daniel-frank-356808.jpg')",
+        "cross-country-image":
+          "url('/assets/img/mika-9IeEFATw1Xk-unsplash.jpg')",
+        "downhill-image":
+          "url('/assets/img/karsten-winegeart-CA0xocGZSFM-unsplash.jpg')",
+        "snowboard-image":
+          "url('/assets/img/colin-lloyd-DvIYbNCZAns-unsplash.jpg')",
+      }),
+      colors: {
+        green: {
+          special: "#5cebf6",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
