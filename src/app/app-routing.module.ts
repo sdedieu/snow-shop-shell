b2227b98e193fffad9b9b2b46e9d285edb85473f
glsm4tt <appstrophe@gmail.com>
2021-08-31 09:00:24 +0200

    ready to demo for angularjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WrapperComponent } from './utils/wrapper/wrapper.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'snowboard', 
+  component: WrapperComponent,
+  data: {
+    remoteName: 'snowboard',
+    remoteEntry: 'http://localhost:4204/remoteEntry.js',
+    exposedModule: './SnowBoardModule',
+    elementName: 'snowboard-page-element'
+  }},
   {path: '', pathMatch: 'full', redirectTo: 'home'}
 ];
 
