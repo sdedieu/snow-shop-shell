62536b92c7f759a8eac1b41306245267a4d9cf0a
glsm4tt <appstrophe@gmail.com>
2021-09-29 08:45:20 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,24 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shell';
-
-  /*
-    constructor(private injector: Injector,
-      private resolver: ComponentFactoryResolver) {
-  }
-  
-  ngAfterViewInit(): void {
-  loadRemoteModule({
-  remoteEntry: 'http://localhost:4205/remoteEntry.js',
-  remoteName: 'snowShopCommon',
-  exposedModule: './Component',
-  })
-  .then(m =>  m.LogoComponent)
-  .then(comp => {
-  const factory = this.resolver.resolveComponentFactory(comp);
-  const component = this.viewContainer.createComponent(factory, undefined, this.injector);
-  component.location.nativeElement.setAttribute('class', 'header-logo')
-  });
-  }
-  */
 }
+
+
+
+
+
+
+
+
+
+/*
+
+  @ViewChild('vc', { read: ViewContainerRef, static: true })
+  viewContainer: ViewContainerRef;
+
+  constructor(private injector: Injector,
+    private resolver: ComponentFactoryResolver) {
+  }
+
+  ngAfterViewInit(): void {
+    loadRemoteModule({
+      remoteEntry: 'http://localhost:4205/remoteEntry.js',
+      remoteName: 'snowShopCommon',
+      exposedModule: './Component',
+    })
+      .then(m => m.LogoComponent)
+      .then(comp => {
+        const factory = this.resolver.resolveComponentFactory(comp);
+        const component = this.viewContainer.createComponent(factory, undefined, this.injector);
+        component.location.nativeElement.setAttribute('class', 'header-logo')
+      });
+  }
+
+  */
