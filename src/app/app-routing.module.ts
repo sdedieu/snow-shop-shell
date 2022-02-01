4c9239f60124bee7717c2b71213c595cd83111c1
sdedieu <dedieu.sylvain38@gmail.com>
2022-02-01 08:23:06 +0100

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,51 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WrapperComponent } from './utils/wrapper/wrapper.component';
-import { loadRemoteModule, LoadRemoteModuleOptions } from '@angular-architects/module-federation';
-
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'home' }
-  
-  /*{ path: 'sales', loadChildren: () => import('mfe/module').then(m => m.SalesModule) },
+  { path: 'sales', loadChildren: () => import('sales/module').then(m => m.SalesModule) },
   {
     path: 'cross-country',
     component: WrapperComponent,
     data: {
-      loadChildren: () => loadRemoteModule({
-        remoteName: 'mfe',
-        remoteEntry: 'http://localhost:4202/remoteEntry.js',
-        exposedModule: './module'
-    }),
+      loadChildren: () => import('crosscountry/module'),
       elementName: 'cross-country-web-component'
     }
-  },
-  {
+  },{
     path: 'downhill',
     component: WrapperComponent,
     data: {
-      loadChildren: () => loadRemoteModule({
-        remoteName: 'mfe',
-        remoteEntry: 'http://localhost:4203/remoteEntry.js',
-        exposedModule: './module'
-    }),
+      loadChildren: () => import('downhill/module'),
       elementName: 'downhill-web-component'
     }
-  },
-  {
+  },{
     path: 'snowboard',
     component: WrapperComponent,
     data: {
-      loadChildren: () => loadRemoteModule({
-        remoteName: 'mfe',
-        remoteEntry: 'http://localhost:4204/remoteEntry.js',
-        exposedModule: './module'
-    }),
+      loadChildren: () => import('snowboard/module'),
       elementName: 'snowboard-web-component'
     }
-  }
-  */
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
