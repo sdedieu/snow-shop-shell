ad325a7e117374d4860eabb2d48f288ae93ef895
sdedieu <dedieu.sylvain38@gmail.com>
2022-02-03 06:50:39 +0100

    add the startWith function for url matcher

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlMatcher, UrlSegment } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+
+export function startsWith(prefix: string): UrlMatcher {
+  return (url: UrlSegment[]) => {
+      const fullUrl = url.map(u => u.path).join('/');
+      if (fullUrl.startsWith(prefix)) {
+          return ({ consumed: url});
+      }
+      return null;
+  };
+}
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
